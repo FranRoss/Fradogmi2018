a505f41a0898d6cd2d4ef94b37fbe633e74509cd
Miorini Iacopo <Iacopo.Miorini@nttdata.com>
2018-03-01 21:02:55 +0100

    Rename class

---
+++ Managers/ChangeAppStatus.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace Fradogmi2018.Managers
+{
+    public class ChangeAppStatus
+    {
+        public ChangeAppStatus()
+        {
+            //Vehicles 
+            for (int i = 0; i < 10; i++)
+            {
+                //Rides 
+                for (int j = 0; j < 10; j++)
+                {
+
+                }
+            }
+        }
+    }
+}
