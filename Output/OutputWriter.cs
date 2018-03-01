d14601dbe4cf696e162e3f9498130ca3e3751792
Rossetto Francesco <Francesco.Rossetto@nttdata.com>
2018-03-01 21:12:58 +0100

    added output

---
+++ Output/OutputWriter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using Fradogmi2018.Entities;
+
+namespace Fradogmi2018.Output
+{
+    public class OutputWriter
+    {
+        public string OutputFileName;
+
+        public OutputWriter(string outputFile)
+        {
+            OutputFileName = outputFile;
+        }
+
+        public void WriteSolution(Vehicle[] vehicles) 
+        {
+            using (StreamWriter stream = new StreamWriter(OutputFileName))
+            {
+                StringBuilder result = new StringBuilder();
+                for (int i = 0; i < vehicles.Length; i++) 
+                {
+                    Vehicle vehicle = vehicles[i];
+                    result.Append(vehicle.RidesToDo.Count);
+                    vehicle.RidesToDo.ForEach(e => 
+                    {
+                        result.Append(' ' + e);
+                    });
+
+                    result.AppendLine();
+                }
+                stream.WriteLine(result);
+            }
+        }
+    }
+}
