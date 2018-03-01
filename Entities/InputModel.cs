523ed8998e2a5aacd30b98e1cce632f25fe623a4
Alex Gola <alex.gola@flyvictor.com>
2018-03-01 18:55:17 +0000

    asdasdasd

--- Entities/InputModel.cs
+++ Entities/InputModel.cs
@@ -18,6 +18,12 @@ namespace Fradogmi2018.Entities
                 string CurrentRow = sr.ReadLine();
                 // 3 rows, 4 columns, 2 vehicles, 3 rides, 2 bonus and 10 steps
                 var values = CurrentRow.Split(' ').Select(e => Convert.ToInt32(e)).ToArray();
+                rows = values[0];
+                columns = values[1]; 
+                vehicles = values[2];
+                rides = values[3];
+                bonus = values[4];
+                steps = values[5];
 
                 for (int i = 0; i < values.Length; i++)
                 {
