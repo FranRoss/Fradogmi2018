696fe8db5bf5028f4b3816990843fe26fc525780
Davide Fedrigo <davidefedri@gmail.com>
2018-03-01 21:11:22 +0100

    no message

--- Managers/ManagersData.cs
+++ Managers/ManagersData.cs
@@ -1,10 +1,30 @@
 ﻿using System;
+using Fradogmi2018.Entities;
+
 namespace Fradogmi2018.Managers
 {
     public class ManagersData
     {
         public ManagersData()
         {
+        }
+
+        public void ManageInputModel(InputModel model){
+          
+            for (int i = 0; i < model.status.TotalSteps; i++)
+            {
+                foreach (var vehicle in model.vehiclesList)
+                {
+                    if(vehicle.Status==VechicleStatus.Free){
+                        foreach (var rides in model.ridesList)
+                        {
+
+                        }
+                    }
+                        
+                        
+                }
+            }
         }
     }
 }
