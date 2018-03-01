25966cf61eb8de199443281c12d3ebfb864bd1de
Rossetto Francesco <Francesco.Rossetto@nttdata.com>
2018-03-01 20:05:08 +0100

    updated appstatus

--- Entities/AppStauts.cs
+++ Entities/AppStauts.cs
@@ -3,8 +3,21 @@ namespace Fradogmi2018.Entities
 {
     public class AppStatus
     {
-        public AppStatus()
+        int Rows;
+        int Columns;
+        int TotalVehicles;
+        int TotalRides;
+        int TotalSteps;
+        int RideBonus;
+
+        public AppStatus(int rows, int columns, int totalVehicles, int totalRides, int rideBonus, int totalSteps)
         {
+          Rows = rows;
+          Columns = columns;
+          TotalVehicles = totalVehicles;
+          TotalRides = totalRides;
+          RideBonus = rideBonus;
+          TotalSteps = totalSteps;
         }
     }
 }
